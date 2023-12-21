c6b0af3fb56e2eff0afcdc47be2a8ff9bea41134
JapiKredi <88824661+JapiKredi@users.noreply.github.com>
2023-12-21 12:28:33 +0800

    Change the prompt

--- openai_function_calling_example.py
+++ openai_function_calling_example.py
@@ -173,7 +173,7 @@ tools = [
     },
 ]
 
-
+"""
 messages = []
 messages.append({"role": "system", "content": "Don't make assumptions about what values to plug into functions. Ask for clarification if a user request is ambiguous."})
 messages.append({"role": "user", "content": "What's the weather like today. I am in Glascow Scotland"})
@@ -182,5 +182,17 @@ chat_response = chat_completion_request(
 )
 assistant_message = chat_response.json()["choices"][0]["message"]
 messages.append(assistant_message)
+print(assistant_message)
 pretty_print_conversation(messages)
+"""
 
+messages = []
+messages.append({"role": "system", "content": "Don't make assumptions about what values to plug into functions. Ask for clarification if a user request is ambiguous."})
+messages.append({"role": "user", "content": "what is the weather going to be like in Glasgow, Scotland over the next x days"})
+chat_response = chat_completion_request(
+    messages, tools=tools
+)
+assistant_message = chat_response.json()["choices"][0]["message"]
+messages.append(assistant_message)
+print(assistant_message)
+pretty_print_conversation(messages)
