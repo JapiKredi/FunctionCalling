cd579604abadfc70ef78a85d30f2d143522b0052
JapiKredi <88824661+JapiKredi@users.noreply.github.com>
2023-12-21 11:26:34 +0800

    Creating a function for exception handling

--- openai_function_calling_example.py
+++ openai_function_calling_example.py
@@ -8,6 +8,7 @@
 
 import json
 import openai
+import os
 import requests
 from tenacity import retry, wait_random_exponential, stop_after_attempt
 from termcolor import colored
@@ -18,3 +19,34 @@ GPT_MODEL = "gpt-3.5-turbo-0613"
 load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
+# the @retry decorator is being used to retry the decorated function in case of failures or exceptions. 
+# It provides a way to automatically retry the function multiple times with a delay between each retry.
+# The wait parameter specifies the wait strategy to use between retries.
+# Delay between retries will be a random exponential backoff with a multiplier of 1 and a maximum delay of 40 seconds.
+# The function will be retried for a maximum of 3 attempts. 
+# If the function still fails after 3 attempts, the exception will be raised.
+@retry(wait=wait_random_exponential(multiplier=1, max=40), stop=stop_after_attempt(3))
+
+
+
+def chat_completion_request(messages, tools=None, tool_choice=None, model=GPT_MODEL):
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer " + openai.api_key,
+    }
+    json_data = {"model": model, "messages": messages}
+    if tools is not None:
+        json_data.update({"tools": tools})
+    if tool_choice is not None:
+        json_data.update({"tool_choice": tool_choice})
+    try:
+        response = requests.post(
+            "https://api.openai.com/v1/chat/completions",
+            headers=headers,
+            json=json_data,
+        )
+        return response
+    except Exception as e:
+        print("Unable to generate ChatCompletion response")
+        print(f"Exception: {e}")
+        return e
