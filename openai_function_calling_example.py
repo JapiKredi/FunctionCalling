ea1f3045ed122ced34f5ce9197f230153d517a88
JapiKredi <88824661+JapiKredi@users.noreply.github.com>
2023-12-21 12:19:01 +0800

    Improved the function as the conversation was not printed. Added pretty print.

--- openai_function_calling_example.py
+++ openai_function_calling_example.py
@@ -122,6 +122,7 @@ def pretty_print_conversation(messages):
 # 5c: required: A list of required parameters. In this case, the required parameters are "location" and "format".
 # The second tool also consists of the property 'num_days': The number of days to forecas
 
+tools = [
     {
         "type": "function",
         "function": {
@@ -171,3 +172,15 @@ def pretty_print_conversation(messages):
         }
     },
 ]
+
+
+messages = []
+messages.append({"role": "system", "content": "Don't make assumptions about what values to plug into functions. Ask for clarification if a user request is ambiguous."})
+messages.append({"role": "user", "content": "What's the weather like today. I am in Glascow Scotland"})
+chat_response = chat_completion_request(
+    messages, tools=tools
+)
+assistant_message = chat_response.json()["choices"][0]["message"]
+messages.append(assistant_message)
+pretty_print_conversation(messages)
+
