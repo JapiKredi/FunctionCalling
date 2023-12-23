f8d62b37ca28c3a01b5baa2588b441e0060414e3
JapiKredi <88824661+JapiKredi@users.noreply.github.com>
2023-12-23 22:24:53 +0800

    Improving

--- function_call_old_code_template.py
+++ function_call_old_code_template.py
@@ -1,3 +1,14 @@
+import openai
+import ast
+import re
+import pandas as pd
+import json
+
+
+# Read the OpenAI Api_key
+openai.api_key = open("OpenAI_API_Key.txt", "r").read().strip()
+
+
 def run_conversation():
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo-0613",
@@ -50,3 +61,6 @@ def run_conversation():
             ],
         )
         return second_response
+    
+
+run_conversation()
