36af75f48d902dcf52b9d91c1ff357a26ec18d45
JapiKredi <88824661+JapiKredi@users.noreply.github.com>
2023-12-23 22:12:38 +0800

    Improving

---
+++ function_call_old_code_template.py
@@ -0,0 +1,52 @@
+def run_conversation():
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo-0613",
+        messages=[{"role": "user", "content": "What's the weather like in Boston?"}],
+        functions=[
+            {
+                "name": "get_current_weather",
+                "description": "Get the current weather in a given location",
+                "parameters": {
+                    "type": "object",
+                    "properties": {
+                        "location": {
+                            "type": "string",
+                            "description": "The city and state, e.g. San Francisco, CA",
+                        },
+                        "unit": {"type": "string", "enum": ["celsius", "fahrenheit"]},
+                    },
+                    "required": ["location"],
+                },
+            }
+        ],
+        function_call="auto",
+    )
+
+    message = response["choices"][0]["message"]
+
+    # Step 2, check if the model wants to call a function
+    if message.get("function_call"):
+        function_name = message["function_call"]["name"]
+        function_args = json.loads(message["function_call"]["arguments"])
+
+        # Step 3, call the function
+        # Note: the JSON response from the model may not be valid JSON
+        function_response = get_current_weather(
+            location=function_args.get("location"),
+            unit=function_args.get("unit"),
+        )
+
+        # Step 4, send model the info on the function call and function response
+        second_response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo-0613",
+            messages=[
+                {"role": "user", "content": "What is the weather like in boston?"},
+                message,
+                {
+                    "role": "function",
+                    "name": function_name,
+                    "content": function_response,
+                },
+            ],
+        )
+        return second_response
